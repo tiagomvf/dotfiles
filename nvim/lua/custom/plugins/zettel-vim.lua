8c6c592ca7236ab015bf85d5abcf6bbb2bec672c
Tiago Maia <tiagomvf@gmail.com>
2025-04-22 19:24:09 -0300

    Config zettel-vim

--- nvim/lua/custom/plugins/zettel-vim.lua
+++ nvim/lua/custom/plugins/zettel-vim.lua
@@ -8,13 +8,11 @@ return {
   'junegunn/fzf.vim',
   {
     'michal-h21/vim-zettel',
-    config = function(_, opts)
+    init = function()
       vim.g.vimwiki_list = {
-        { path = '~/junk/wiki', ext = '.md', syntax = 'markdown' },
-        { path = '~/junk/zettel', ext = '.md', syntax = 'markdown' },
+        { path = '~/zettel', ext = '.md', syntax = 'markdown' },
       }
       vim.g.zettel_options = {
-        { zettel_format = '%y%m%d%H%M%' },
         { zettel_format = '%y%m%d%H%M%' },
       }
     end,
